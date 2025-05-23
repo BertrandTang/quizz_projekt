ed1d3c5d42822ef9e0190bc2899bf021f222688d
Bertrand <bertrand.tang.dev@gmail.com>
2025-05-23 11:09:39 +0200

    updates styles

--- src/components/FilterQuizz.jsx
+++ src/components/FilterQuizz.jsx
@@ -11,19 +11,22 @@ export default function FilterQuiz({ category, onCategoryChange }) {
     const categories = [...new Set(stateQuizzData.map(question => question.category))];
 
     return (
-        <>
-          <div className="p-2">Filtrer par catégorie </div>
-        <Form.Select
-        aria-label="Category select"
-        value={category}
-        // On envoie category à App.jsx via l'écouteur d'évènement onCategoryChange 
-            onChange={event => onCategoryChange(event.target.value)}
-        >
-            <option value="0">Toutes</option>
-            {categories.map((cat, index) =>
-                <option key={index} value={cat}>{cat}</option>
-            )}
-        </Form.Select>
-        </>
+        <Form className='mt-4'>
+            <Form.Group>
+                <Form.Label>Filtrer par catégorie</Form.Label>
+                <Form.Select
+                    aria-label="Category select"
+                    value={category}
+                    // On envoie category à App.jsx via l'écouteur d'évènement onCategoryChange 
+                    onChange={event => onCategoryChange(event.target.value)}
+                    className="mb-3"
+                >
+                    <option value="0">Toutes</option>
+                    {categories.map((cat, index) =>
+                        <option key={index} value={cat}>{cat}</option>
+                    )}
+                </Form.Select>
+            </Form.Group>
+        </Form>
     );
 }
